420d4d028a1b131815f1c7bded9dd3bc3e5765ef
WPDev-Mac-Rojab <irojabkhan@gmail.com>
2022-04-12 00:37:23 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="content">
+                    <h1>Hi, I'm Rojab Khan ;)</h1>
+                    <p>I am a professional web developer and passionate photographer based on Dhaka.</p>
+                    <a href="www.facebook.com">Download Resume</a>
+                </div>
+                <div className="footer">
+                    <a href="www.facebook.com" target="_blank">Github</a>
+                    <a href="www.facebook.com" target="_blank">Twitter</a>
+                    <a href="www.facebook.com" target="_blank">Linkedin</a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
