078e04bf8efbc3edd0efe6fc4136880d3195bb36
WPDev-Mac-Rojab <irojabkhan@gmail.com>
2022-04-12 00:58:23 +0600

    title change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import './asset/resume.pdf';
 function App() {
     return (
         <div className="App">
@@ -6,12 +6,12 @@ function App() {
                 <div className="content">
                     <h1>Hi, I'm Rojab Khan ;)</h1>
                     <p>I am a professional web developer and passionate photographer based on Dhaka.</p>
-                    <a href="www.facebook.com">Download Resume</a>
+                    <a href="./asset/resume.pdf" target="_blank" download>Download Resume</a>
                 </div>
                 <div className="footer">
-                    <a href="www.facebook.com" target="_blank">Github</a>
-                    <a href="www.facebook.com" target="_blank">Twitter</a>
-                    <a href="www.facebook.com" target="_blank">Linkedin</a>
+                    <a href="https://github.com/irojabkhan" target="_blank" rel="noreferrer">Github</a>
+                    <a href="https://twitter.com/irojabkhan" target="_blank" rel="noreferrer">Twitter</a>
+                    <a href="https://www.linkedin.com/in/rojab-khan-482779136/" target="_blank" rel="noreferrer">Linkedin</a>
                 </div>
             </div>
         </div>
